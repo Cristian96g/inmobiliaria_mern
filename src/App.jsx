363b1e37414af705063193320feff77e73fea3a4
Cristian Gomez <cristian.gomez.r1089@gmail.com>
2023-11-29 15:44:52 -0300

    creando paginas y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 function App() {
 
 
   return (
-    <>
-      <h1 className='text-3xl text-center'>Home</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
